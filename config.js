486ae343ab48d957fb7a2dfba83f92eae5eb4b3a
Frank Wallis <fwallis@outlook.com>
2015-06-19 08:23:57 +0100

    intitial check-in

---
+++ config.js
@@ -0,0 +1,20 @@
+System.config({
+  "baseURL": "/",
+  "defaultJSExtensions": true,
+  "transpiler": "typescript",
+  "paths": {
+    "github:*": "jspm_packages/github/*"
+  },
+  "packages": {
+    "app": {
+      "defaultExtension": "ts"
+    }
+  }
+});
+
+System.config({
+  "map": {
+    "typescript": "github:mhegazy/typescript@v1.5-beta2"
+  }
+});
+
